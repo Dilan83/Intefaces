e4d6e9eb424815543cd4e568f4668e261c6a7c8f
d.sakiz <d.sakiz@IR1-PC-17.musterschule.schule.paedml>
2018-02-19 08:45:52 +0100

    Schlechte Idee

--- src/interfaces/Interfaces.java
+++ src/interfaces/Interfaces.java
@@ -17,9 +17,8 @@ public class Interfaces {
     public static void main(String[] args) {
         // TODO code application logic here
         int a=5;
-        for (int i = 0; i < 10; i++) {
-            
+        int b=10; 
         }
     }
     
-}
+
